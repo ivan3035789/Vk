c625e50ef277b6b604549fddc2598712cdfb2e8b
ivan3035789 <82367614+ivan3035789@users.noreply.github.com>
2021-06-22 17:12:15 +0300

    added package manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    // + get/set на все поля
+
+    public Post[] search(int ownerId, String domain, String query, int count, int offset, boolean ownersOnly1) {
+        return null;
+    }
+
+
+    public void delete(int ownerId, boolean postId) {
+
+    }
+}
